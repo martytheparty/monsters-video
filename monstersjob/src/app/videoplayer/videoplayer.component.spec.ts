fea9f6ac039f836b923befb3ca659536fa03cb01
Marty Trujillo <marty@ilikeemail.com>
2020-01-18 00:16:48 -0500

    Fixes the last unit tests.

--- monstersjob/src/app/videoplayer/videoplayer.component.spec.ts
+++ monstersjob/src/app/videoplayer/videoplayer.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+
 
 import { VideoplayerComponent } from './videoplayer.component';
+import { VideoListComponent } from '../video-list/video-list.component';
+
+
 
 describe('VideoplayerComponent', () => {
   let component: VideoplayerComponent;
@@ -8,7 +18,17 @@ describe('VideoplayerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VideoplayerComponent ]
+      declarations: [
+        VideoplayerComponent,
+        VideoListComponent
+       ],
+      imports: [
+        MatIconModule,
+        MatCardModule,
+        MatChipsModule,
+        RouterTestingModule,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
   }));
@@ -19,7 +39,7 @@ describe('VideoplayerComponent', () => {
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
