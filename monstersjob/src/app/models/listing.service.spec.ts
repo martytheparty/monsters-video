8a1366e39c832e5111371f38d66d56a58f6ee595
Marty Trujillo <marty@ilikeemail.com>
2020-01-17 23:56:14 -0500

    Fixes a broken unit test.

--- monstersjob/src/app/models/listing.service.spec.ts
+++ monstersjob/src/app/models/listing.service.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 import { ListingService } from './listing.service';
 
 describe('ListingService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  }));
 
-  xit('should be created', () => {
+  it('should be created', () => {
     const service: ListingService = TestBed.get(ListingService);
     expect(service).toBeTruthy();
   });
