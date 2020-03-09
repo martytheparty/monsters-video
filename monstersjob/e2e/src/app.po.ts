a39e97b9c082e797456dd634df62f61b5a944ac7
Marty <marty@ilikeemail.com>
2020-03-08 20:37:27 -0400

    removes old/broken e2e tests

--- monstersjob/e2e/src/app.po.ts
+++ monstersjob/e2e/src/app.po.ts
@@ -5,7 +5,5 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
-  }
+
 }
